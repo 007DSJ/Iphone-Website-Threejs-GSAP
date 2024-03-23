84bd997cbb0378ed996dc41684e47c57dadf68e3
007DSJ <shinobigamerr565@gmail.com>
2024-03-23 18:42:46 +0530

    Model Layout created

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -4,6 +4,9 @@ import ModelView from "./ModelView";
 import { useRef, useState } from "react";
 import { yellowImg } from "../utils";
 import * as THREE from 'three';
+import { models, sizes } from "../constants";
+import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
 
 
 const Model = () =>{
@@ -43,7 +46,58 @@ const Model = () =>{
 
                 <div className="flex flex-col items-center mt-5">
                     <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-                        <ModelView/>
+                        <ModelView
+                            index={1}
+                            groupRef={small}
+                            gsapType="view1"
+                            controlRef={cameraControlSmall}
+                            setRotationState={setSmallRotation}
+                            item={model}
+                            size={size}
+                        />
+                        <ModelView
+                            index={2}
+                            groupRef={large}
+                            gsapType="view2"
+                            controlRef={cameraControlLarge}
+                            setRotationState={setLargeRotation}
+                            item={model}
+                            size={size}
+                        />
+
+                        <Canvas className='w-full h-full' 
+                                style={{ position: 'fixed', top:0, bottom:0, left:0, right:0, overflow: 'hidden'}}
+                                eventSource={document.getElementById('root')}
+                        >
+                            <View.Port/>
+                        </Canvas>
+                    </div>
+                    <div className="mx-auto w-full">
+                        <p className="text-sm font-light text-center mb-5">{model.title}</p>
+                        <div className="flex-center">
+                            <ul className="color-container">
+                                {models.map((item, i)=>(
+                                    <li key={i}
+                                     className="w-6 h-6 rounded-full mx-2 cursour-pointer"
+                                     style={{ backgroundColor: item.color[0]}}
+                                     onClick={() => setModel(item)}
+                                    >
+
+                                    </li>
+                                ))}
+                            </ul>
+
+                            <button className="size-btn-container">
+                                { sizes.map(({label, value})=>(
+                                    <span key={label} className="size-btn-container" 
+                                            style={{backgroundColor : size === value ? 'white' : 'transparent', color : size===value ? 'black' : 'white'}}
+                                            onClick={()=>setSize(value)}
+                                            >
+                                        {label}
+                                    </span>
+                                ))}
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,18 +1,19 @@
 import { useGSAP } from "@gsap/react";
+import { View } from "@react-three/drei";
 import gsap from "gsap";
 
-const ModelView = () =>{
-
-    useGSAP(()=>{
-
-        gsap.to('#heading',{ y:0, opacity:1})
-
-    },[])
+const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size, item}) =>{
 
     return(
-        <section className="common-padding">
-          
-        </section>
+        <View
+            index={index}
+            id={gsapType}
+            className={`border-2 border-red-500 w-full h-full ${index===2} ? 'right-[-100%] : ''`}
+        >
+            {/* ambient light */}
+            <ambientLight intensity={0.3}/>
+            
+        </View>
     )
 }
 
